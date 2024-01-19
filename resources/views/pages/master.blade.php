4654a394040bf749546372fb27cd0dde1fc15344
dianeib <59963593+dianeib@users.noreply.github.com>
2024-01-19 09:12:24 +0000

    .

--- resources/views/pages/master.blade.php
+++ resources/views/pages/master.blade.php
@@ -19,7 +19,6 @@
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11" defer></script>
 {{-- <script src="sweetalert2.min.js"></script>
 <link rel="stylesheet" href="sweetalert2.min.css"> --}}
-
 	<link rel="stylesheet" href="{{ asset('css/animate.css') }}">
 
 	<link rel="stylesheet" href="{{ asset('css/owl.carousel.min.css') }}">
