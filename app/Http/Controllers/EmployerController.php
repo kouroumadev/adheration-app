d3db05fbcb3c3a0ac70367e435b626ddc02f3630
kouroumadev <57208103+kouroumadev@users.noreply.github.com>
2024-01-19 11:32:55 +0000

    .

--- app/Http/Controllers/EmployerController.php
+++ app/Http/Controllers/EmployerController.php
@@ -98,7 +98,7 @@ class EmployerController extends Controller
         );
 
 
-        $headers = ['nom_employer','prenom_employer','sexe_employer','matricule','adresse_employer','email_employer','n_immatriculation','date_naissance_employer','lieu_naissance_employer','nationalite','prenom_pere','prenom_mere','nom_pere','nom_mere','situation_matrimoniale','profession','n_cin','date_del_cin','lieu_del_cin','n_acte_naissance','date_del_acte_naissance','lieu_del_acte_naissance','etat_employer','date_embauche','date_immatriculation','liberer'];
+        $headers = ['nom_employer','prenom_employer','sexe_employer','matricule','adresse_employer','email_employer','n_immatriculation','date_naissance_employer','lieu_naissance_employer','pays_naissance_employer','nationalite','ville_employer','quartier_employer','commune_employer','tel_employer','situation_matrimoniale','profession','n_cin','date_del_cin','lieu_del_cin','date_embauche','salaire_brut','emploi_occupe','liberer'];
 
         if(Input::hasFile('employee_file')){
             $headings = (new HeadingRowImport)->toArray(request()->file('employee_file'));
