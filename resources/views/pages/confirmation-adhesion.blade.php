4465ad7b881e467863451b393874da1d35f33e25
kouroumadev <57208103+kouroumadev@users.noreply.github.com>
2024-01-21 13:13:42 +0000

    Update confirmation-adhesion.blade.php

--- resources/views/pages/confirmation-adhesion.blade.php
+++ resources/views/pages/confirmation-adhesion.blade.php
@@ -4,7 +4,7 @@
 <section class="ftco-section" style="margin-top: 100px">
     <div class="card w-50 m-auto" style="border: none">
         <h3 class="text-center mb-4 text-success font-weight-bold">Demande Soumise</h3>
-        <p class="text-center font-weight-bold text-dark">Nous vous demandons de bien voiloir telecharger votre demande d'adhesion au service E-CNSS et de vous rendre a votre
+        <p class="text-center font-weight-bold text-dark">Nous vous demandons de bien vouloir telecharger votre demande d'adhesion au service E-CNSS et de vous rendre a votre
             agence de rattachement CNSS pour achever votre demande.
         </p>
 
